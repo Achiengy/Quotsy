755339a093f6c8953943e415204b827518c6cce0
Achiengy <petehope7@gmail.com>
2023-03-29 08:53:19 +0300

    initial commit

---
+++ src/main/java/com/quotsy/myfavequotsy/MyfavequotsyApplication.java
@@ -0,0 +1,13 @@
+package com.quotsy.myfavequotsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfavequotsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfavequotsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quotsy/myfavequotsy/MyfavequotsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quotsy.myfavequotsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfavequotsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
