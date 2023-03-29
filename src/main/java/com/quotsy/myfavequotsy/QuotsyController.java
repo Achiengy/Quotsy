0f0ff271ed377317c1f9abcbcd487cb69a63184b
Achiengy <petehope7@gmail.com>
2023-03-29 10:00:22 +0300

    ft:quote

--- src/main/java/com/quotsy/myfavequotsy/QuotsyController.java
+++ src/main/java/com/quotsy/myfavequotsy/QuotsyController.java
@@ -1,8 +1,13 @@
 package com.quotsy.myfavequotsy;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuotsyController {
-    
+
+    @GetMapping("/quotsy")
+    public String word(){
+         return "Remember to live every day to the fullest!";
+}
 }
